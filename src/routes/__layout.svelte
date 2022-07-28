f6ebc6966e3258d3f282d9f2b1f68ae4dbe0ff03
Nathan Gaul <nathanjgaul@gmail.com>
2022-07-28 18:49:09 -0500

    init

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Header from '$lib/header/Header.svelte';
+	import '../app.scss';
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		const bootstrap = await import('bootstrap');
+	})
+</script>
+
+<main>
+	<slot />
+</main>
+
+<style>
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<div class="container py-4 px-3 mx-auto">
+		<h1>Hello, Bootstrap and SvelteKit!</h1>
+		<button class="btn btn-primary">Primary button</button>
+	</div>
+</section>
+
+<style>
+</style>
